ca236ba2a89c8169716b323b6759d34a60807908
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-03-31 23:10:15 +0900

    로그인 시 비밀번호 검증로직 추가

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -9,11 +9,44 @@ const createHashedPassword = async (password) => {
     return hashedPassword;
 };
 
+const comparePassword = async (inputPassword, hashedPassword) => {
+    try {
+        const match = await bcrypt.compare(inputPassword, hashedPassword);
+        return match;
+    } catch (error) {
+        throw error;
+    }
+}
+
 exports.signUp = async (userId, userPw) => {
     try {
         const hashedPassword = await createHashedPassword(userPw);
         await db.query(userRepository.signUp, [userId, hashedPassword]);
 
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.login = async (userId, userPw) => {
+    try {
+        const userData = await db.query(userRepository.getUserById, [userId]);
+
+        if (!userData || !userData.length) {
+            return false;
+        }
+
+        // DB에 저장된 비밀번호 부분
+        const hashedPassword = userData[0].password;
+
+        // 비밀번호 검증
+        const passwordMatch = await comparePassword(inputPassword, hashedPassword);
+
+        if (passwordMatch) {
+            return true;
+        } else {
+            return false;
+        }
     } catch (error) {
         throw error;
     }
